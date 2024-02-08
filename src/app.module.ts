93e95a865131bad895039688631a814830ee39ba
edcastillob <edwar.castillo@gmail.com>
2024-02-08 15:55:45 -0400

    Backend + documentación swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { DataSourceConfig } from './config/data.source';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './auth/auth.module';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:`.${process.env.NODE_ENV.trim()}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({...DataSourceConfig }),
+    UsersModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
